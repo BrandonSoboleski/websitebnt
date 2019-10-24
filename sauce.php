11a5b67ecc42362b32c83eac31cd3b35ce5a21c9
BrandonSoboleski <bsoboleski2@gmail.com>
2019-10-24 18:52:30 -0400

    Add files via upload

---
+++ sauce.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.11.3, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.11.3, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo4.png" type="image/x-icon">
+  <meta name="description" content="Prebuilt page for the 6-12 sandwich builder.">
+  
+  <title>6-12 Sauce Page</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<style>
+.container {
+  position: relative;
+  width: 65%;
+  max-width: 400px;
+}
+
+.container img {
+  width: 60%;
+  height: auto;
+}
+
+.container .btn {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, 140%);
+  -ms-transform: translate(-50%, -50%);
+  background-color: #555;
+  color: white;
+  font-size: 16px;
+  padding: 12px 24px;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+  text-align: center;
+}
+
+.container .btn:hover {
+  background-color: black;
+}
+.column {
+  float: left;
+  width: 33.33%;
+  padding: 5px;
+}
+
+/* Clear floats after image containers */
+.row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+</style>
+<script type="text/javascript">
+  function allownext()
+  {
+    document.getElementById("btnnext").href="confirmation.php"; 
+  }
+</script>
+<body>
+  <section class="header5 cid-rFLInF4iNo mbr-fullscreen" id="header5-9">
+    
+
+    <div class="row" id="optiongrid">
+      <div class="row">
+        <h1 class="mbr-section-title align-center pb-3 mbr-fonts-style display-1"  style="padding-left: 400px;"> Choose Sauces to Add on to your Sandwich</h1>
+      </div>
+      <div class="column">
+        <div class="container">
+          <img src="img_mustard.jpg" alt="sandwich" style="width:100%; padding-bottom: 60px;padding-left: 10px;">
+          <a class="btn btn-md btn-white-outline display-4" onclick="allownext()" style="width:100%; height:40px;">Mustard</a>
+        </div>
+        <div class="container">
+          <img src="img_mustard.jpg" alt="sandwich" style="width:100%; padding-bottom: 60px;padding-left: 10px;">
+          <a class="btn btn-md btn-white-outline display-4" onclick="allownext()" style="width:100%; height:40px;">Mustard</a>
+        </div>
+        <div class="container">
+          <img src="img_mustard.jpg" alt="sandwich" style="width:100%; padding-bottom: 60px;padding-left: 10px;">
+          <a class="btn btn-md btn-white-outline display-4" onclick="allownext()" style="width:100%; height:40px;">Mustard</a>
+        </div>
+        <div class="container">
+          <img src="img_mustard.jpg" alt="sandwich" style="width:100%; padding-bottom: 60px;padding-left: 10px;">
+          <a class="btn btn-md btn-white-outline display-4" onclick="allownext()" style="width:100%; height:40px;">Mustard</a>
+        </div>
+        <div class="container">
+          <img src="img_mustard.jpg" alt="sandwich" style="width:100%; padding-bottom: 60px;padding-left: 10px;">
+          <a class="btn btn-md btn-white-outline display-4" onclick="allownext()" style="width:100%; height:40px;">Mustard</a>
+        </div>
+      </div>
+      <div class="column">
+        <div class="container">
+          <img src="img_mustard.jpg" alt="sandwich" style="width:100%; padding-bottom: 60px;padding-left: 10px;">
+          <a class="btn btn-md btn-white-outline display-4" onclick="allownext()" style="width:100%; height:40px;">Mustard</a>
+        </div>
+        <div class="container">
+          <img src="img_mustard.jpg" alt="sandwich" style="width:100%; padding-bottom: 60px;padding-left: 10px;">
+          <a class="btn btn-md btn-white-outline display-4" onclick="allownext()" style="width:100%; height:40px;">Mustard</a>
+        </div>
+        <div class="container">
+          <img src="img_mustard.jpg" alt="sandwich" style="width:100%; padding-bottom: 60px;padding-left: 10px;">
+          <a class="btn btn-md btn-white-outline display-4" onclick="allownext()" style="width:100%; height:40px;">Mustard</a>
+        </div>
+        <div class="container">
+          <img src="img_mustard.jpg" alt="sandwich" style="width:100%; padding-bottom: 60px;padding-left: 10px;">
+          <a class="btn btn-md btn-white-outline display-4" onclick="allownext()" style="width:100%; height:40px;">Mustard</a>
+        </div>
+        <div class="container">
+          <img src="img_mustard.jpg" alt="sandwich" style="width:100%; padding-bottom: 60px;padding-left: 10px;">
+          <a class="btn btn-md btn-white-outline display-4" onclick="allownext()" style="width:100%; height:40px;">Mustard</a>
+        </div>
+      </div>
+      <div class="column">
+        <div class="container">
+          <img src="img_mustard.jpg" alt="sandwich" style="width:100%; padding-bottom: 60px;padding-left: 10px;">
+          <a class="btn btn-md btn-white-outline display-4" onclick="allownext()" style="width:100%; height:40px;">Mustard</a>
+        </div>
+        <div class="container">
+          <img src="img_mustard.jpg" alt="sandwich" style="width:100%; padding-bottom: 60px;padding-left: 10px;">
+          <a class="btn btn-md btn-white-outline display-4" onclick="allownext()" style="width:100%; height:40px;">Mustard</a>
+        </div>
+        <div class="container">
+          <img src="img_mustard.jpg" alt="sandwich" style="width:100%; padding-bottom: 60px;padding-left: 10px;">
+          <a class="btn btn-md btn-white-outline display-4" onclick="allownext()" style="width:100%; height:40px;">Mustard</a>
+        </div>
+        <div class="container">
+          <img src="img_mustard.jpg" alt="sandwich" style="width:100%; padding-bottom: 60px;padding-left: 10px;">
+          <a class="btn btn-md btn-white-outline display-4" onclick="allownext()" style="width:100%; height:40px;">Mustard</a>
+        </div>
+        <div class="container">
+          <img src="img_mustard.jpg" alt="sandwich" style="width:100%; padding-bottom: 60px;padding-left: 10px;">
+          <a class="btn btn-md btn-white-outline display-4" onclick="allownext()" style="width:100%; height:40px;">Mustard</a>
+        </div>
+	  </div>
+  </div>
+  
+	<div class="container" id="conttainnext">
+			<a class="btn btn-md btn-white-outline display-4"   style="width:300px;height:40px;background-color: #de1822;" id="btnnext">Next</a>
+	</div>
+
+</section>
+
+
+  <section class="engine"><a href="https://mobirise.info/v">free html templates</a></section><script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  
+  
+</body>
+</html>
